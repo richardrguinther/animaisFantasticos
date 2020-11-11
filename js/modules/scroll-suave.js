e172b30d51e191718963f9fcddf421d2b6da7510
unknown <richard.rguinther@gmail.com>
2020-11-10 21:23:49 -0300

    ScrollSuave converted into class

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,27 +1,33 @@
-export default function initScrollSuave() {
-  const linksInternos = document.querySelectorAll(
-    '[data-tab="menu"] a[href^="#"]'
-  );
+export default class ScrollSuave {
+  constructor(links, options) {
+    this.linksInternos = document.querySelectorAll(links);
+    if (options === undefined) {
+      this.options = { behavior: "smooth", block: "start" };
+    } else {
+      this.options = options;
+    }
+    console.log(this.scrollToSection.bind(this));
+    this.scrollToSection = this.scrollToSection.bind(this);
+  }
 
-  function scrollToSection(event) {
+  scrollToSection(event) {
     event.preventDefault();
     const href = event.currentTarget.getAttribute("href");
     const section = document.querySelector(href);
 
-    section.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-
-    // Forma Alternativa
-    // const topo = section.offsetTop;
-    // window.scrollTo({
-    //     top: topo,
-    //     behavior: 'smooth'
-    // });
+    section.scrollIntoView(this.options);
   }
 
-  linksInternos.forEach((item) => {
-    item.addEventListener("click", scrollToSection);
-  });
+  addLinkEvent() {
+    this.linksInternos.forEach((item) => {
+      item.addEventListener("click", this.scrollToSection);
+    });
+  }
+
+  init() {
+    if (this.links.length) {
+      this.addLinkEvent();
+    }
+    return this;
+  }
 }
